543b167e90f400f983084aad794d5098420ca6e9
Somers Matthews <somersbmatthews@gmail.com>
2020-02-04 23:49:10 -0600

    made endpoints file

---
+++ instacook-user/main.go
@@ -0,0 +1,47 @@
+// StringService provides operations on strings.
+import "context"
+
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+
+import (
+	"context"
+	"errors"
+	"strings"
+)
+
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
